dfd755e0528f94f0897ae210f29dc12873e1c053
jirwincline <jci@pdx.edu>
2020-04-28 16:21:38 -0700

    add relations in models

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,6 @@
 class Entry < ApplicationRecord
+    belongs_to :report, required: false
+
+    validates :body, presence: true
+    validates :time, presence: true
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,7 @@
 class Report < ApplicationRecord
   belongs_to :user
+  has_many :entries, dependent: :destroy
+
+  # Could put validates
+  # validates :body, presence: true
 end
