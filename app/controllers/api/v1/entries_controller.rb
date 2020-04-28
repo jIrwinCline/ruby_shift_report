4892d76fa23cd6c7a1ef9bf558bec40a9d9499ba
jirwincline <jci@pdx.edu>
2020-04-28 16:13:29 -0700

    add modules for api versioning

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,51 +1,55 @@
-class EntriesController < ApplicationController
-  before_action :set_entry, only: [:show, :update, :destroy]
+module Api
+  module V1
+    class EntriesController < ApplicationController
+    before_action :set_entry, only: [:show, :update, :destroy]
 
-  # GET /entries
-  def index
-    @entries = Entry.all
+    # GET /entries
+    def index
+      @entries = Entry.all
 
-    render json: @entries
-  end
-
-  # GET /entries/1
-  def show
-    render json: @entry
-  end
-
-  # POST /entries
-  def create
-    @entry = Entry.new(entry_params)
-
-    if @entry.save
-      render json: @entry, status: :created, location: @entry
-    else
-      render json: @entry.errors, status: :unprocessable_entity
+      render json: @entries
     end
-  end
 
-  # PATCH/PUT /entries/1
-  def update
-    if @entry.update(entry_params)
+    # GET /entries/1
+    def show
       render json: @entry
-    else
-      render json: @entry.errors, status: :unprocessable_entity
+    end
+
+    # POST /entries
+    def create
+      @entry = Entry.new(entry_params)
+
+      if @entry.save
+        render json: @entry, status: :created, location: @entry
+      else
+        render json: @entry.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /entries/1
+    def update
+      if @entry.update(entry_params)
+        render json: @entry
+      else
+        render json: @entry.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /entries/1
+    def destroy
+      @entry.destroy
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_entry
+        @entry = Entry.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def entry_params
+        params.require(:entry).permit(:body, :report)
+      end
     end
   end
-
-  # DELETE /entries/1
-  def destroy
-    @entry.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_entry
-      @entry = Entry.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def entry_params
-      params.require(:entry).permit(:body, :report)
-    end
 end
