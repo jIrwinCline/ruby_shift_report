db228df7a4734c04b836b779b60095d72240fb01
jirwincline <jci@pdx.edu>
2020-04-29 13:07:12 -0700

    add create react app frontend

---
+++ app/shift_report_frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//removed
+//removed
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trimmed React App</h1>
+    </div>
+  );
+}
+
+export default App;
