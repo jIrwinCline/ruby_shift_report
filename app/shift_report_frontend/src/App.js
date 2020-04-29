eb19023aaea00a776e3662de7a90eb19484e6ddc
jirwincline <jci@pdx.edu>
2020-04-29 15:07:20 -0700

    add pages folder and all necessary pages for app

--- app/shift_report_frontend/src/App.js
+++ app/shift_report_frontend/src/App.js
@@ -6,15 +6,20 @@ import {
   Route,
 } from "react-router-dom";
 
-import Login from "./components/Login";
-import Register from "./components/Register";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ReportsPage from "./pages/ReportsPage";
+import CreateReportPage from "./pages/CreateReportPage";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+        <Route exact path="/reports" component={ReportsPage} />
+        <Route exact path="/report/entries" component={CreateReportPage} />
+        Page
       </Switch>
     </Router>
   );
