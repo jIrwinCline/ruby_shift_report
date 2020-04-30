d7a338344e79520ce3b611c4a0d74f69f4d7b3e6
jirwincline <jci@pdx.edu>
2020-04-29 18:58:21 -0700

    add useContext hook instead of importing the context component

--- app/shift_report_frontend/src/pages/CurrentUserPage.js
+++ app/shift_report_frontend/src/pages/CurrentUserPage.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import UserContext from "../context/user-context";
 
 export default function CurrentUserPage(props) {
+  const context = useContext(UserContext);
+
+  useEffect(() => {
+    console.log(context);
+  }, []);
+
   return (
-    <UserContext.Consumer>
-      {(context) => (
-        <React.Fragment>
-          <button onClick={context.signin}>Sign in</button>
-          <button onClick={context.logout}>Logout</button>
-          <div>Hello {context.test}</div>
-        </React.Fragment>
-      )}
-    </UserContext.Consumer>
+    <React.Fragment>
+      <button onClick={context.signin}>Sign in</button>
+      <button onClick={context.logout}>Logout</button>
+      <div>Hello {context.test}</div>
+    </React.Fragment>
   );
 }
