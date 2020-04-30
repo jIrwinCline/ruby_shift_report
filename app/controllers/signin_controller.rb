469dd28265ba0ba78b725d0d02ba7cabd7d8c69a
jirwincline <jci@pdx.edu>
2020-04-30 15:25:29 -0700

    fixed typo in signin controller and now that returns token as well.

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -6,7 +6,7 @@ class SigninController < ApplicationController
 
         if user.authenticate(params[:password])
             payload = {user_id: user.id}
-            session = JWTSession::Session.new(payload: payload, refresh_by_access_allowed: true)
+            session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
             tokens = session.login
             response.set_cookie(JWTSessions.access_cookie,
                                 value: tokens[:access],
