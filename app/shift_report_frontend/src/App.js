2aa33957b6db73b80ccd1d1716aa0c956567e579
jirwincline <jci@pdx.edu>
2020-04-29 17:43:26 -0700

    add some context to work with state

--- app/shift_report_frontend/src/App.js
+++ app/shift_report_frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext } from "react";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -12,17 +12,41 @@ import ReportsPage from "./pages/ReportsPage";
 import CreateReportPage from "./pages/CreateReportPage";
 import CurrentUserPage from "./pages/CurrentUserPage";
 
+import UserContext from "./context/user-context";
+
+const initialState = {
+  user: null,
+  reports: [],
+  test: "Teststate",
+};
+
+const signin = () => {
+  console.log("signed in");
+};
+const logout = () => {
+  console.log("signed out");
+};
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={CurrentUserPage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/register" component={RegisterPage} />
-        <Route exact path="/reports" component={ReportsPage} />
-        <Route exact path="/report/entries" component={CreateReportPage} />
-      </Switch>
-    </Router>
+    <UserContext.Provider
+      value={{
+        ...initialState,
+        signin: signin(),
+        logout: logout(),
+      }}
+    >
+      return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={CurrentUserPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/reports" component={ReportsPage} />
+          <Route exact path="/report/entries" component={CreateReportPage} />
+        </Switch>
+      </Router>
+      );
+    </UserContext.Provider>
   );
 }
 
--- app/shift_report_frontend/src/pages/CurrentUserPage.js
+++ app/shift_report_frontend/src/pages/CurrentUserPage.js
@@ -1,5 +1,10 @@
 import React from "react";
+import UserContext from "../context/user-context";
 
-export default function CurrentUserPage() {
-  return <div>Hello (current user)</div>;
+export default function CurrentUserPage(props) {
+  return (
+    <UserContext.Consumer>
+      {(context) => <div>Hello {context.test}</div>}
+    </UserContext.Consumer>
+  );
 }
