ff27a1c8eee2c9c63e0bfbaf731ca5fb8921d4f9
jirwincline <jci@pdx.edu>
2020-04-30 22:51:57 -0700

    add login front end html pieces

--- app/shift_report_frontend/src/pages/LoginPage.js
+++ app/shift_report_frontend/src/pages/LoginPage.js
@@ -1,5 +1,33 @@
 import React from "react";
 
 export default function Login() {
-  return <div>login</div>;
+  // const [] = useState()
+  const handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    console.log("signedin");
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <h2>Sign in</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          name="email"
+          onChange={handleChange}
+          placeholder="test@test.com"
+          type="text"
+        />
+        <input
+          name="password"
+          onChange={handleChange}
+          placeholder="password"
+          type="text"
+        />
+        <input />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
 }
