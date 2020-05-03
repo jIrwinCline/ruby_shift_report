61afe9584539c3f43f7bb0b417ae298dd836631c
jirwincline <jci@pdx.edu>
2020-05-03 16:30:50 -0700

    add a route for generating doc files

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -43,6 +43,10 @@ module Api
       @report.destroy
     end
 
+    def generate
+      puts "generate Route"
+    end
+
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_report
