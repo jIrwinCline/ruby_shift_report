f45c86e5c0ec4608f29e55d3e4772acd9b763770
jirwincline <jci@pdx.edu>
2020-05-05 18:55:29 -0700

    add content to generate_doc function, but running into major problem with 1) not being able to add header content and 2) generating templates with iframes is throwing errors

--- app/models/report.rb
+++ app/models/report.rb
@@ -10,23 +10,38 @@ class Report < ApplicationRecord
     report = self
     user = User.find(self.user_id)
     
-    Caracal::Document.save 'example.docx' do |doc|
-      doc.h1 'PORTLAND PROJECT 
+    Caracal::Document.save 'example.docx' do |docx|
+      docx.style do 
+        id 'header'
+        name 'header'
+        font 'Times New Roman'
+        size '48'
+      end
+      
+      docx.style do 
+        id 'body'
+        name 'body'
+        font 'Times New Roman'
+        size '24'
+      end
+      
+      docx.h1 'PORTLAND PROJECT 
       PORTLAND CENTER PLAZA
       THE LINC
       '
-      doc.hr
-      doc.h1 'SUNDAY, APRIL 12th , 2020'
-      doc.page
+      docx.hr
+      docx.h1 'SUNDAY, APRIL 12th , 2020'
+      docx.page
       entries.each do |entry|
-        doc.p entry.body
+        docx.p entry.body
+        docx.p
       end
       
-      doc.h2 'Section 1'
-      doc.p 'Lorem ipsum dolor....'
-      doc.p
+      docx.h2 'Section 1'
+      docx.p 'Lorem ipsum dolor....'
+      docx.p
     end
     { report: report, entries: entries, user: user}
   end
-
+  
 end
