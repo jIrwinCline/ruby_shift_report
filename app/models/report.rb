fccf7fa09fc96243e3aa6af2f7e751d53ea5e0b0
jirwincline <jci@pdx.edu>
2020-05-07 10:20:09 -0700

    It will add date correctly to first page

--- app/models/report.rb
+++ app/models/report.rb
@@ -10,7 +10,9 @@ class Report < ApplicationRecord
     entries = self.entries
     report = self
     user = User.find(self.user_id)
-    
+    # date = Date.new(report["timestamp"])
+    date = Date.today
+
     doc = Docx::Document.open("template.docx")
     docxEntries = []
     # doc.bookmarks['day'].insert_after("SATURDAY")
@@ -18,7 +20,18 @@ class Report < ApplicationRecord
       docxEntries.push(nil) 
       docxEntries.push(entry)
     end
-    doc.bookmarks['day'].insert_text_after("SATURDAY")
+    doc.paragraphs.each do |p|
+      p.each_text_run do |tr|
+        if tr.text == "SATURDAY"
+          tr.text=(date.strftime("%^A"))
+        end
+        if tr.text == ", APRIL 12th , 2020"
+         
+          tr.text=(", #{date.strftime("%^B")} #{date.strftime("%-d")} , #{date.strftime("%Y")}")
+        end
+      end
+    end
+    # doc.bookmarks['day'].insert_text_after("SATURDAY")
     doc.bookmarks['start'].insert_multiple_lines(docxEntries.map {|entry| entry ? "#{entry.time}    #{entry.body}" : "" })
     # entries.each do |entry|
     #   doc.bookmarks['start'].insert_text_after("#{entry.body}")
