977ac6a0d5295c192dc729779569eb954bd00364
jirwincline <jci@pdx.edu>
2020-05-06 17:28:59 -0700

    tried docx.ifram but getting "missing end of zip file" error

--- app/models/report.rb
+++ app/models/report.rb
@@ -11,35 +11,36 @@ class Report < ApplicationRecord
     user = User.find(self.user_id)
     
     Caracal::Document.save 'example.docx' do |docx|
-      docx.style do 
-        id 'header'
-        name 'header'
-        font 'Times New Roman'
-        size '48'
-      end
+        docx.style do 
+          id 'header'
+          name 'header'
+          font 'Times New Roman'
+          size '48'
+        end
       
-      docx.style do 
-        id 'body'
-        name 'body'
-        font 'Times New Roman'
-        size '24'
-      end
+        docx.style do 
+          id 'body'
+          name 'body'
+          font 'Times New Roman'
+          size '24'
+        end
       
-      docx.h1 'PORTLAND PROJECT 
-      PORTLAND CENTER PLAZA
-      THE LINC
-      '
-      docx.hr
-      docx.h1 'SUNDAY, APRIL 12th , 2020'
-      docx.page
-      entries.each do |entry|
-        docx.p entry.body
+        docx.h1 'PORTLAND PROJECT 
+        PORTLAND CENTER PLAZA
+        THE LINC
+        '
+        docx.hr
+        docx.h1 'SUNDAY, APRIL 12th , 2020'
+        docx.page
+        entries.each do |entry|
+          docx.p entry.body
+          docx.p
+        end
+      
+        docx.h2 'Section 1'
+        docx.p 'Lorem ipsum dolor....'
         docx.p
-      end
-      
-      docx.h2 'Section 1'
-      docx.p 'Lorem ipsum dolor....'
-      docx.p
+      # docx.iframe data: File.read('snippet.docx')
     end
     { report: report, entries: entries, user: user}
   end
