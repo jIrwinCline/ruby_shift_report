85c94c83e2b799295e3d53170260ebea630761ce
jirwincline <jci@pdx.edu>
2020-05-07 09:30:41 -0700

    insertssaturday, but formatting is an issue

--- app/models/report.rb
+++ app/models/report.rb
@@ -13,11 +13,12 @@ class Report < ApplicationRecord
     
     doc = Docx::Document.open("template.docx")
     docxEntries = []
-    
+    # doc.bookmarks['day'].insert_after("SATURDAY")
     entries.each_with_index do |entry, index|
       docxEntries.push(nil) 
       docxEntries.push(entry)
     end
+    doc.bookmarks['day'].insert_text_after("SATURDAY")
     doc.bookmarks['start'].insert_multiple_lines(docxEntries.map {|entry| entry ? "#{entry.time}    #{entry.body}" : "" })
     # entries.each do |entry|
     #   doc.bookmarks['start'].insert_text_after("#{entry.body}")
