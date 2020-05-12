af444aa21906375d0f05f2e70d828f61f9d4754f
jirwincline <jci@pdx.edu>
2020-05-12 16:08:03 -0700

    added material UI door on user page (/)

--- app/shift_report_frontend/src/pages/CurrentUserPage.js
+++ app/shift_report_frontend/src/pages/CurrentUserPage.js
@@ -2,9 +2,24 @@ import React, { useContext, useEffect } from "react";
 import AppContext from "../context/app-context";
 import { useHistory } from "react-router-dom";
 
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+import Drawer from "../components/Drawer";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
 export default function CurrentUserPage(props) {
   const context = useContext(AppContext);
   const history = useHistory();
+  const classes = useStyles();
 
   useEffect(() => {
     if (!window.localStorage.signedIn) {
@@ -14,19 +29,22 @@ export default function CurrentUserPage(props) {
 
   return (
     <div className="user-page-container">
-      <button onClick={() => context.logout(history)}>Logout</button>
-      <div>
-        <p>Hello {context.currentUser.email}</p>
-        <p> {context.currentUser.fname}</p>
-        <p> {context.currentUser.dpsst}</p>
+      <Drawer />
+      <div className={classes.content}>
+        <button onClick={() => context.logout(history)}>Logout</button>
+        <div>
+          <p>Hello {context.currentUser.email}</p>
+          <p> {context.currentUser.fname}</p>
+          <p> {context.currentUser.dpsst}</p>
+        </div>
+        <button
+          onClick={() => {
+            context.startReport(history, context.currentUser);
+          }}
+        >
+          Create Report
+        </button>
       </div>
-      <button
-        onClick={() => {
-          context.startReport(history, context.currentUser);
-        }}
-      >
-        Create Report
-      </button>
     </div>
   );
 }
