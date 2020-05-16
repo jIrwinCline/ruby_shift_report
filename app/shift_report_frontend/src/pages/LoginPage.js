5816cd2c258b2fc5f0d8493a9d8a48466702f640
jirwincline <jci@pdx.edu>
2020-05-16 13:08:46 -0700

    add login form to be used and repurpose login functionality into that component

--- app/shift_report_frontend/src/pages/LoginPage.js
+++ app/shift_report_frontend/src/pages/LoginPage.js
@@ -1,40 +1,11 @@
-import React, { useContext, useReducer, useState } from "react";
-import AppContext from "../context/app-context";
-import { useHistory } from "react-router-dom";
+import React from "react";
+
+import LoginForm from "../components/loginForm/LoginForm";
 
 export default function Login(props) {
-  const context = useContext(AppContext);
-  const history = useHistory();
-  const [credentials, setCredentials] = useState({ email: "", password: "" });
-  const handleChange = (event) => {
-    setCredentials({ ...credentials, [event.target.name]: event.target.value });
-  };
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    await context.signin({
-      email: credentials.email,
-      password: credentials.password,
-    });
-    await history.push("/");
-  };
   return (
-    <div>
-      <h2>Sign in</h2>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          name="email"
-          onChange={handleChange}
-          placeholder="test@test.com"
-          type="text"
-        />
-        <input
-          name="password"
-          onChange={handleChange}
-          placeholder="password"
-          type="text"
-        />
-        <button>Log in</button>
-      </form>
+    <div style={{ paddingTop: "5vh" }}>
+      <LoginForm />
     </div>
   );
 }
