7ef368e5c0f1649d6cd9a8232d7accda56d23358
jirwincline <jci@pdx.edu>
2020-05-15 19:18:07 -0700

    implement grid on current user page

--- app/shift_report_frontend/src/pages/CurrentUserPage.js
+++ app/shift_report_frontend/src/pages/CurrentUserPage.js
@@ -3,8 +3,10 @@ import AppContext from "../context/app-context";
 import { useHistory } from "react-router-dom";
 
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import TitleText from "../components/TitleText/TitleText";
+import Grid from "@material-ui/core/Grid";
 
+import CustomPaper from "../components/CustomPaper/CustomPaper";
+import TitleText from "../components/TitleText/TitleText";
 import Drawer from "../components/Drawer";
 
 const useStyles = makeStyles((theme) => ({
@@ -33,18 +35,24 @@ export default function CurrentUserPage(props) {
       <Drawer />
       <div className={classes.content}>
         <TitleText>Dashboard</TitleText>
-        <div>
-          <p>Hello {context.currentUser.email}</p>
-          <p> {context.currentUser.fname}</p>
-          <p> {context.currentUser.dpsst}</p>
-        </div>
-        <button
-          onClick={() => {
-            context.startReport(history, context.currentUser);
-          }}
-        >
-          Create Report
-        </button>
+        <Grid container spacing={2}>
+          <Grid item xs={9}>
+            <CustomPaper>
+              <p>Hello {context.currentUser.email}</p>
+              <p> {context.currentUser.fname}</p>
+              <p> {context.currentUser.dpsst}</p>
+            </CustomPaper>
+          </Grid>
+          <Grid item xs={3}>
+            <button
+              onClick={() => {
+                context.startReport(history, context.currentUser);
+              }}
+            >
+              Create Report
+            </button>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
