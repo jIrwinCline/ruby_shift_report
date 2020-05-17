2730e6b06522c34ef2d50d13cd9d5189e674d85d
jirwincline <jci@pdx.edu>
2020-05-17 12:12:20 -0700

    det change and key events to save new state of body withn component

--- app/shift_report_frontend/src/components/EntryItem.js
+++ app/shift_report_frontend/src/components/EntryItem.js
@@ -3,10 +3,21 @@ import TextField from "@material-ui/core/TextField";
 
 export default function EntryItem(props) {
   const [editMode, setEditMode] = useState(false);
+  const [newBody, setNewBody] = useState(null);
+  let inputEnter = document.getElementById("new-body");
+  // inputEnter.onkeyup =
   let editItem = editMode ? (
     <TextField
+      onKeyUp={(e) => {
+        if (e.keyCode == 13) {
+          alert(newBody);
+        }
+      }}
+      onChange={(e) => {
+        setNewBody(e.target.value);
+      }}
       onBlur={() => setEditMode(!editMode)}
-      id="standard-full-width"
+      id="new-body"
       style={{ margin: 8 }}
       defaultValue={props.item.body}
       fullWidth
