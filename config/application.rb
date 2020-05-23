b1749ae39faa6d344241e70a1c5170abcf616acc
jirwincline <jci@pdx.edu>
2020-05-22 20:39:36 -0700

    .

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module ShiftReport
     config.load_defaults 6.0
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://stage1.dpa1hp3vejn1k.amplifyapp.com/'
+        origins 'https://stage1.dpa1hp3vejn1k.amplifyapp.com'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://stage1.dpa1hp3vejn1k.amplifyapp.com/'
+    origins 'https://stage1.dpa1hp3vejn1k.amplifyapp.com'
     
     resource '*',
       headers: :any,
